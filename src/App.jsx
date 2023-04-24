e84486c94441df97deaa1d77075728fe4a534c0b
Erik Alvar <erikalvarbass@gmail.com>
2023-04-24 18:56:44 -0400

    adds task to list via useState

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,38 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
+  const [task, setTask] = useState("")
+  const [list, setList] = useState([])
 
+  console.log(list)
+
+  const handleChange = (e) => {
+    setTask(e.target.value)
+  }
+
+  const handleSubmit= (e) => {
+    e.preventDefault()
+    setList((prev) => [...prev, task])
+    console.log(list)
+  }
+  const listItems = list.map(item => <li key={item}>{item}</li>)
+ 
   return (
     <>
-      <p>
-        test clear
-      </p>
+    <form onSubmit={handleSubmit}>
+      <input
+        type="text"
+        value={task}
+        onChange={handleChange}
+      >
+      </input>
+      <button >Submit</button>
+    </form>
+    <p></p>
+    <ul>
+      {listItems}
+    </ul>
+      
     </>
   )
 }
